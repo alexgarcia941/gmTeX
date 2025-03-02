b280e215ff32be6ade15a00ca069e3f5ef98cafb
alexgarcia941 <alexgarcia28098@gmail.com>
2025-03-02 14:02:07 -0800

    almost done with core functionality

--- src/content.js
+++ src/content.js
@@ -1,21 +1,42 @@
 import * as InboxSDK from '@inboxsdk/core';
+import katex from "katex";
 
+//functions
+function renderLatex(latex) {
+    try {
+        return katex.renderToString(latex, { throwOnError: false });   
+    } catch (error) {
+        console.error("KaTeX rendering error", error);
+        return latex;
+    }
+}
 
+function replaceLatexWithRendered(text) {
+    const latexRegex = /\$\s*([^$]+?)\s*\$/g;
+    if (latexRegex.test(text)) {
+        return text.replace(latexRegex, (_, latex) => renderLatex(latex))
+    } else {
+        return text
+    }
+}
+
+//Objects
 const latexButton = {
     title: "Render TeX",
     iconUrl: "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
 
     //feels like a crime
     onClick(event) {
-        event.composeView.insertTextIntoBodyAtCursor("this is only the beginning")
+        const html = event.composeView.getHTMLContent();
+        const renderedText = replaceLatexWithRendered(html);
+        event.composeView.setBodyHTML(renderedText);
     }
 }
 
+//run
 InboxSDK.load(2, "Hello World")
     .then( (sdk) => {
         sdk.Compose.registerComposeViewHandler((composeView) => {
             composeView.addButton(latexButton);
         });
     });
-
-
